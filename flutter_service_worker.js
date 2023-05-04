8a8ff46d85af2e6a13ac9050da6b1741dc14572a
azothazoth <92980855+azothazoth@users.noreply.github.com>
2023-05-04 17:18:03 +0800

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -127,9 +127,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b6655e410eb127698a9569ddd18372dc",
-"/": "b6655e410eb127698a9569ddd18372dc",
-"main.dart.js": "4a3cb6b7e7a896cf680f18e14cff46d9",
+"index.html": "b78b4fe56543740d0b73ac59ddf461c5",
+"/": "b78b4fe56543740d0b73ac59ddf461c5",
+"main.dart.js": "e8b7e36cb17d9c9976d99b98eff92027",
 "manifest.json": "c95146b47ed79efca72edf2dba078390",
 "version.json": "35a4df3de8e123efa436bc8c745735e8"
 };
